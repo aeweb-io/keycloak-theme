560ccd089f93e4c03a406c76a8253698d8beeac6
Adam L Edwards <5575331+adamledwards@users.noreply.github.com>
2023-05-03 11:18:51 +0100

    initial

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/admin');
+  await page.goto('/realms/BMFS/account/#/security/signingin');
 
   // Expect a title "to contain" a substring.
   await expect(page.getByRole('heading', {level: 1})).toHaveText('Sign in');
 });
 
 test('username and password', async ({ page }) => {
-    await page.goto('/admin');
+    await page.goto('/realms/BMFS/account/#/security/signingin');
 
   // Click the get started link.
   await expect(page.getByLabel('username or email')).toBeVisible()
@@ -16,7 +16,7 @@ test('username and password', async ({ page }) => {
 });
 
 test('username and password invalid', async ({ page }) => {
-    await page.goto('/admin');
+    await page.goto('/realms/BMFS/account/#/security/signingin');
 
     await page.getByRole('button', {name: 'Sign in'}).click()
     await expect(page.getByRole('alert')).toContainText('There is a problem')
